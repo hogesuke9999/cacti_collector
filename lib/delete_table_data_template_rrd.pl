f5b7bbd8dbc5a087734dc060aaa6363f9d596911
Hogesuke9999 <keisuke@e-nyanko.net>
2015-12-28 10:42:17 +0900

    削除処理の追加

--- lib/delete_table_data_template_rrd.pl
+++ lib/delete_table_data_template_rrd.pl
@@ -32,16 +32,16 @@ sub delete_table_data_template_rrd {
 	my $sql_w, $sth_w, $arr_w_ref;
 	my $sql_w2, $sth_w2, $arr_w2_ref;
 
-	print "***** Delete Table Name = data_template_data_rra *****\n";
+	print "***** Delete Table Name = data_template_rrd *****\n";
 
-	$sql_w = "select ref_id from data_template_data_rra where ref_hostname = '" . $db_r_host . "';";
+	$sql_w = "select ref_id from data_template_rrd where ref_hostname = '" . $db_r_host . "';";
 	$sth_w = $db_w->prepare($sql_w);
 	$sth_w->execute;
 
 	while (my $arr_ref = $sth_w->fetchrow_arrayref) {
 		my ($TABLE_ref_id) = @$arr_ref;
 
-		$sql_r = "select count(*) from data_template_data_rra where id = '" . $TABLE_ref_id . "';";
+		$sql_r = "select count(*) from data_template_rrd where id = '" . $TABLE_ref_id . "';";
 		$sth_r = $db_r->prepare($sql_r);
 		$sth_r->execute;
 
@@ -50,7 +50,7 @@ sub delete_table_data_template_rrd {
 		$sth_r->finish;
 
 		if($id_exist < 1) {
-			$sql_w2 = "delete from data_template_data_rra where ref_id = '" . $TABLE_ref_id . "' and ref_hostname = '" . $db_r_host . "';";
+			$sql_w2 = "delete from data_template_rrd where ref_id = '" . $TABLE_ref_id . "' and ref_hostname = '" . $db_r_host . "';";
 			print "Delete : " . $sql_w . "\n";
 	        	$db_w->do($sql_w2);
 		}
