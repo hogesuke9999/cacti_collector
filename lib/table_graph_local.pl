2439ab68dbb34689b58f78cad9f00078d993979e
Hogesuke9999 <keisuke@e-nyanko.net>
2016-01-01 00:08:28 +0900

    graph_local 確認・修正

--- lib/table_graph_local.pl
+++ lib/table_graph_local.pl
@@ -17,7 +17,9 @@ sub copy_table_graph_local {
 	my ( $db_w, $db_r, $db_r_host) = @_;
 
 	my $sql_r, $sth_r, $arr_r_ref;
+	my $sql_r2, $sth_r2, $arr_r2_ref;
 	my $sql_w, $sth_w, $arr_w_ref;
+	local $snmp_query_id;
 
 	print "***** Copy Table Name = graph_local *****\n";
 
@@ -29,15 +31,24 @@ sub copy_table_graph_local {
 #			snmp_index
 #		from graph_local;";
 
+#	$sql_r = "select
+#			graph_local.id,
+#			graph_templates.hash,
+#			graph_local.host_id,
+#			snmp_query.hash,
+#			graph_local.snmp_index
+#		from graph_local, graph_templates, snmp_query
+#		where graph_local.graph_template_id = graph_templates.id
+#		  and graph_local.snmp_query_id = snmp_query.id ;";
+
 	$sql_r = "select
 			graph_local.id,
 			graph_templates.hash,
 			graph_local.host_id,
-			snmp_query.hash,
+			graph_local.snmp_query_id,
 			graph_local.snmp_index
-		from graph_local, graph_templates, snmp_query
-		where graph_local.graph_template_id = graph_templates.id
-		  and graph_local.snmp_query_id = snmp_query.id ;";
+		from graph_local, graph_templates
+		where graph_local.graph_template_id = graph_templates.id;";
 
 	$sth_r = $db_r->prepare($sql_r);
 	$sth_r->execute;
@@ -47,9 +58,15 @@ sub copy_table_graph_local {
 			$TABLE_id,
 			$TABLE_graph_templates_hash,
 			$TABLE_host_id,
-			$TABLE_snmp_query_hash,
+			$TABLE_snmp_query_id,
 			$TABLE_snmp_index
 		) = @$arr_ref;
+
+print "DEBUG:data_graph_local TABLE_id = "                   . $TABLE_id                   . "\n";
+print "DEBUG:data_graph_local TABLE_graph_templates_hash = " . $TABLE_graph_templates_hash . "\n";
+print "DEBUG:data_graph_local TABLE_host_id = "              . $TABLE_host_id              . "\n";
+print "DEBUG:data_graph_local TABLE_snmp_query_id = "        . $TABLE_snmp_query_id        . "\n";
+print "DEBUG:data_graph_local TABLE_snmp_index = "           . $TABLE_snmp_index           . "\n";
 
 		# 変換後のhost_idの取得
 		#  変換前 : $TABLE_host_id
@@ -68,12 +85,25 @@ sub copy_table_graph_local {
 		my ($graph_template_id) = @$arr_w_ref;
 		$sth_w->finish;
 
-		$sql_w = "select id from snmp_query where hash = '';";
-		$sth_w = $db_w->prepare($sql_w);
-		$sth_w->execute;
-		my $arr_w_ref = $sth_w->fetchrow_arrayref;
-		my ($snmp_query_id) = @$arr_w_ref;
-		$sth_w->finish;
+		if($TABLE_snmp_query_id == 0) {
+			$snmp_query_id = 0;
+		} else {
+			$sql_r2 = "select hash from snmp_query where id = '" . $TABLE_snmp_query_id . "';";
+			$sth_r2 = $db_r->prepare($sql_r2);
+			$sth_r2->execute;
+			$arr_r2_ref = $sth_r2->fetchrow_arrayref;
+			($TABLE_snmp_query_hash) = @$arr_r2_ref;
+			$sth_r2->finish;
+
+			$sql_w = "select id from snmp_query where hash = '" . $TABLE_snmp_query_hash . "';";
+			$sth_w = $db_w->prepare($sql_w);
+			$sth_w->execute;
+			my $arr_w_ref = $sth_w->fetchrow_arrayref;
+			my ($snmp_query_id) = @$arr_w_ref;
+			$sth_w->finish;
+		}
+
+print "DEBUG:data_graph_local snmp_query_id = " . $snmp_query_id . "\n";
 
 	        $sql_w = "select count(*) from graph_local
 	        where host_id = '" .           $NEW_host_id . "'
